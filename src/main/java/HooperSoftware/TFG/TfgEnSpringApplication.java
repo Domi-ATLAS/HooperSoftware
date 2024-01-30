3d1f2cfd19eef447a193ecaf6bdc3d20bcbd6e18
Domi-ATLAS <edupriv2002@gmail.com>
2024-01-30 20:27:02 +0100

    inicializacion del proyecto

---
+++ src/main/java/HooperSoftware/TFG/TfgEnSpringApplication.java
@@ -0,0 +1,13 @@
+package HooperSoftware.TFG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgEnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgEnSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HooperSoftware/TFG/TfgEnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package HooperSoftware.TFG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgEnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
